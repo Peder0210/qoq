adf40fec7bc5b1592bd3b4a24e57c001e1a4c7b8
Kasper K. Andersen <31669585+illu12@users.noreply.github.com>
2020-02-15 18:53:23 +0100

    Update server.js
    /create har en dynamisk struktur med adskillige attributter til at oprette og indsætte et opslag i databasen, derudover kan der foretages GET requests til endpoint.

--- server.js
+++ server.js
@@ -18,26 +18,34 @@ let db = mongoose.connection;
 
 // Retrieve posts
 app.get("/browse",function(req,res){
-
   // Get all books
   var all_books = db.collection("books").find();
-
   console.log(all_books);
-
   res.send("Done w. BROWSING");
 });
 
 
 // Create item
 app.get("/create",function(req,res){
+
+  // Create item
   let item = {
-    "name": "Dragoon Balls",
-    "ISBN": "7864201337"
+    "name": req.query.name,
+    "isbn": req.query.isbn,
+    "pages": req.query.pages,
+    "price": req.query.price,
+    "language": req.query.language,
+    "author": req.query.author,
+    "rental_period": req.query.rental_period,
+    "book_material": req.query.book_material
   }
-  db.collection("books").insert(item);
-  
-  res.setHeader("Content-Type","application/json");
-  res.end(JSON.stringify(req.query, null, 2));  
+
+  // Add to database
+  db.collection("books").insertOne(item);
+
+  // Return a response
+  res.setHeader("Content-Type","text/plain");
+  res.end("Item created:\n\n"+JSON.stringify(item, null, 2));
 });
 
 
