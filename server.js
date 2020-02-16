d75756ab5c0c34a04044e76895d7751d5b9acf30
Kasper K. Andersen <31669585+illu12@users.noreply.github.com>
2020-02-16 23:30:10 +0100

    Update server.js
    Minify'ed

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ const path = require("path");
 
 // Initialize express object
 const app = express();
-
 // Declare parser
 app.use(bodyParser.urlencoded({extended:true}));
-
 // Make connection
 mongoose.connect("mongodb://localhost:5000/items");
-
 // Get reference
 let db = mongoose.connection;
-
 // Retrieve posts
 app.get("/browse",function(req,res){
 
